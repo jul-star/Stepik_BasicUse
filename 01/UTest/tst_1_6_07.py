b70b5b6c5568d30283143bae6a59ff69c592d7a6
Julian Starchenko <jstar@jinr.ru>
2019-12-23 21:07:37 +0300

    Working on 1_6_7

--- 01/UTest/tst_1_6_07.py
+++ 01/UTest/tst_1_6_07.py
@@ -1,21 +1,140 @@
 import unittest
 from ex_1_6_07 import *
+from unittest.mock import patch
+
 
 class TestCase_1_6_06(unittest.TestCase):
-    def Check(self, Inheritance, Request, Expected):
-        c = Connection()
-        Actual = c.FindConnection(Inheritance, Request)
-        self.assertEqual(len(Expected), len(Actual))
-        for i in range(len(Expected)):
-            self.assertEqual(Expected[i], Actual[i])
+    def test_ReadInput_1(self):
+        _list = [2, 'A : C B', 'B : D E']
+        with patch('builtins.input', side_effect=_list):
+            Actual = Connection.ReadInput()
+            Expected = [['A', ':', 'C', 'B'], ['B', ':', 'D', 'E']]
+            self.assertListEqual(Actual, Expected)
+
+    def test_ReadInput_2(self):
+        _list = [4, 'A', 'B : A', 'C : A', 'D : B C']
+        with patch('builtins.input', side_effect=_list):
+            Actual = Connection.ReadInput()
+            Expected = [['A'], ['B', ':', 'A'], ['C', ':', 'A'], ['D', ':', 'B', 'C']]
+            self.assertListEqual(Actual, Expected)
+
+    def test_ReadInput_3(self):
+        _list = [2, 'A C', 'B E']
+        with patch('builtins.input', side_effect=_list):
+            Actual = Connection.ReadInput()
+            Expected = [['A', 'C'], ['B', 'E']]
+            self.assertListEqual(Actual, Expected)
+
+    def test_getInheritance_1(self):
+        _raw = [['A', ':', 'C', 'B'], ['B', ':', 'D', 'E']]
+        _Actual = Connection.getInheritance(_raw)
+        _Expected = [['A', 'C', 'B'], ['B', 'D', 'E']]
+        self.assertListEqual(_Actual, _Expected)
+
+    def test_getInheritance_2(self):
+        _raw = [['A'], ['B', ':', 'A'], ['C', ':', 'A'], ['D', ':', 'B', 'C']]
+        _Actual = Connection.getInheritance(_raw)
+        _Expected = [['A'], ['B', 'A'], ['C', 'A'], ['D', 'B', 'C']]
+        self.assertListEqual(_Actual, _Expected)
+
+    def test_BuildInheritance_1(self):
+        _inh = [['A', 'C', 'B'], ['B', 'D', 'E']]
+        _Actual = Connection.BuildInheritance(_inh)
+        _Expected = {'A': ['C', 'B'], 'B': ['D', 'E']}
+        self.assertDictEqual(_Actual, _Expected)
+
+    def test_BuildInheritance_2(self):
+        _inh = [['A'], ['B', 'A'], ['C', 'A'], ['D', 'B', 'C']]
+        _Actual = Connection.BuildInheritance(_inh)
+        _Expected = {'A': [], 'B': ['A'], 'C': ['A'], 'D': ['B', 'C']}
+        self.assertDictEqual(_Actual, _Expected)
+
+    def test_getRequest_1(self):
+        _raw = [['E' 'A']]
+        _Actual = Connection.getRequest(_raw)
+        _Expected = [['E' 'A']]
+        self.assertListEqual(_Actual, _Expected)
+
+    def test_getRequest_2(self):
+        _raw = [['E', 'A'], ['B' ,'A'], ['C' ,'A'], ['D', 'A']]
+        _Actual = Connection.getRequest(_raw)
+        _Expected = [['E' 'A'], ['B' 'A'], ['C' 'A'], ['D' 'A']]
+        self.assertListEqual(_Actual, _Expected)
+
+    def test_BuildRequest_1(self):
+        _raw = [['E' 'A']]
+        _Actual = Connection.BuildRequest(_raw)
+        _Expected = [['E', 'A']]
+        self.assertDictEqual(_Actual, _Expected)
+
+    def test_BuildRequest_2(self):
+        _raw = [['E' 'A'], ['B' 'A'], ['C' 'A'], ['D' 'A']]
+        _Actual = Connection.BuildRequest(_raw)
+        _Expected = [['E', 'A'], ['B', 'A'], ['C', 'A'], ['D', 'A']]
+        self.assertDictEqual(_Actual, _Expected)
+
+    def Check(self, inheritance: dict, requests: list, expected: list) -> None:
+        """
+        Helper Function
+        :param Inheritance: B is a child of A
+        :param Request:  Is A parent of B?
+        :param Expected: True/False
+        :return: None
+        """
+        _Actual = Connection.FindInheritance(inheritance, requests)
+        self.assertListEqual(_Actual, expected)
 
     def test_case_01(self):
-        Inheritance = [['A', 'C', 'B'], ['B', 'D', 'E']]
-        Request = ['E' 'A']
+        Inheritance = {'A': [], 'B': ['A']}
+        Requests = [['A', 'B']]
         Expected = [True]
-        self.Check(Inheritance, Request, Expected)
+        self.Check(Inheritance, Requests, Expected)
+
+    def test_case_02(self):
+        Inheritance = {'A': [], 'B': ['A'], 'C': ['A'], 'D': ['B', 'C']}
+        Requests = [['A', 'B'], ['B', 'D'], ['C', 'D'], ['D', 'A']]
+        Expected = [True, True, True, False]
+        self.Check(Inheritance, Requests, Expected)
+
+    def test_case_03(self):
+        Inheritance = {'A': ['C', 'B'], 'B': ['D', 'E']}
+        Requests = [['E', 'A']]
+        Expected = [True]
+        self.Check(Inheritance, Requests, Expected)
+
+    def test_case_04(self):
+        Inheritance = {  # список введённых строк
+            'G': ['F'],
+            # сначала отнаследуем от F, потом его объявим, корректный алгоритм все равно правильно обойдёт граф, независимо что было раньше: наследование или объявление
+            'A': [],
+            'B': ['A'],
+            'C': ['A'],
+            'D': ['B', 'C'],
+            'E': ['D'],
+            'F': ['D'],
+            # а теперь другая ветка наследования
+            'X': [],
+            'Y': ['X', 'A'],
+            # свяжем две ветки наследования для проверки, обошла ли рекурсия предков Z и предков Y в поисках A
+            'Z': ['X'],
+            'V': ['Z', 'Y'],
+            'W': ['V']
+        }
+
+        Requests = [  # список введённых запросов
+            ['A', 'G'],  # Yes   # A предок G через B/C, D, F
+            ['A', 'Z'],  # No    # Y потомок A, но не Y
+            ['A', 'W'],  # Yes   # A предок W через Y, V
+            ['X', 'W'],  # Yes   # X предок W через Y, V
+            ['X', 'QWE'],  # No    # нет такого класса QWE
+            ['A', 'X'],  # No    # классы есть, но они нет родства :)
+            ['X', 'X'],  # Yes   # родитель он же потомок
+            ['1', '1'],  # No    # несуществующий класс
+        ]
+
+        Expected = [True, False, True, True, False, False, True, False]
+        self.Check(Inheritance, Requests, Expected)
 
 
-
-
-
+if __name__ == '__main__':
+    unittest.main()
--- 01/src/ex_1_6_07.py
+++ 01/src/ex_1_6_07.py
@@ -1,88 +1,107 @@
 class Connection:
+    find = False
 
-    def __init__(self):
-        self.find = False
-        self.inh = []
-        self.req = {}
     @staticmethod
     def ReadInput():
         n = int(input())
         lst = []
         for i in range(n):
-            lst.append(input().split())
+            line = input().rstrip()
+            _lst = line.split()
+            lst.append(_lst)
         return lst
-    @staticmethod
-    def ReadInheritance():
-        return Connection.ReadInput()
-    @staticmethod
-    def ReadRequest():
-        return Connection.ReadInput()
 
-    """
-    :param Inheritance - list of list [[a, b, c], [a,d,e]]
-    """
     @staticmethod
-    def _BuildInheritance(Inheritance):
+    def getInheritance(_raw: list) -> list:
+        _inh = []
+        for l in _raw:
+            _i = []
+            for i in l:
+                if i == ':':
+                    continue
+                else:
+                    _i.append(i)
+            _inh.append(_i)
+        return _inh
+
+    @staticmethod
+    def getRequest(_raw: list) -> list:
+        return _raw
+
+    @staticmethod
+    def BuildInheritance(inheritance: list) -> dict:
         inh = {}
-        for lst in Inheritance:
+        for lst in inheritance:
             j = 0
             for c in lst:
                 if j == 0:
                     child = c
                     inh[child] = []
-                if j > 1:
+                else:
                     inh[child].append(c)
                 j += 1
         return inh
-    @staticmethod
-    def _BuildRequest(Request):
-        req = {}
-        for child, parent in Request:
-            req[child]=parent
 
+    @staticmethod
+    def BuildRequest(request: list) -> list:
+        req = []
+        for ln in request:
+            for parent, child in ln:
+                req.append([parent, child])
         return req
 
-    def _find_inheritance(self):
+    @staticmethod
+    def FindInheritance(_inheritance: dict, requests: list) -> list:
         res = []
-        for parent, child in self.req.items():
-            self._check_parent(parent, child)
-            if self.find:
+        for r in requests:
+            parent, child = r
+            Connection._check_parent(_inheritance, parent, child)
+            if Connection.find:
                 res.append(True)
-                self.find = False
+                Connection.find = False
             else:
                 res.append(False)
         return res
 
-    def _check_parent(self, parent, child):
-        if child in self.inh.keys():
-            if parent in self.inh[child]:
-                find = True
-                return True;
+
+    @staticmethod
+    def _check_parent(_inheritance: dict, parent, child):
+        if child == parent and child in _inheritance.keys():
+            Connection.find = True
+            return True
+        if child in _inheritance.keys():
+            if parent in _inheritance[child]:
+                Connection.find = True
+                return True
             else:
-                for cand in self.inh[child]:
-                    if cand in self.inh.keys():
-                        self._check_parent(parent, cand)
+                for candidate in _inheritance[child]:
+                    if candidate in _inheritance.keys():
+                        Connection._check_parent(_inheritance, parent, candidate)
         else:
             return False
         return False
 
-    def FindConnection(self, Inheritance, Request):
-        self.inh = Connection._BuildInheritance(Inheritance)
-        self.req = Connection._BuildRequest(Request)
-        print("Inheritance: ", self.inh)
-        print("Requests: ", self.req)
 
-        return self._find_inheritance()
+    @staticmethod
+    def FindConnection():
+        _tmp = Connection.ReadInput()
+        _raw_inh = Connection.getInheritance(_tmp)
+        _tmp = Connection.ReadInput()
+        _raw_req = Connection.getRequest(_tmp)
+        _inheritance = Connection.BuildInheritance(_raw_inh)
+        _requests = Connection.BuildRequest(_raw_req)
+        _res = Connection.FindInheritance(_inheritance, _requests)
+        Connection.printResult(_res)
 
-    def Find(self):
-        Inheritance = Connection.ReadInheritance()
-        Request = Connection.ReadRequest()
-        res = self.FindConnection(Inheritance, Request)
+
+    def printResult(self, res):
         for i in res:
             if i:
                 print("Yes")
             else:
                 print("No")
-        return
 
 
+
+if __name__ == "__main__":
+    Connection.FindConnection()
