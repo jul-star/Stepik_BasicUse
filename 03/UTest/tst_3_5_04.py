c165bfed507332387e4fefa3106369188f1f667a
Julian Starchenko <jstar@jinr.ru>
2019-12-26 21:27:47 +0300

    Done course

--- 03/UTest/tst_3_5_04.py
+++ 03/UTest/tst_3_5_04.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch
+from io import StringIO
 from ex_3_5_04 import Descendants
 
 
@@ -7,102 +8,116 @@ class TestCase_3_5_04(unittest.TestCase):
     def test_getInput_1(self):
         _user_input = [
             '[{"name": "A", "parents": []}, \
-             {"name": "B", "parents": ["A", "C"]}, \
-             {"name": "C", "parents": ["A"]}]']
+            {"name": "B", "parents": ["A", "C"]}, \
+            {"name": "C", "parents": ["A"]}]']
         _Actual = []
-        _Expected = [{'A': []},
-                     {'B': ['A', 'C']},
-                     {'C': ['A']}]
+        _Expected = {'A': [],
+                     'B': ['A', 'C'],
+                     'C': ['A']}
         with patch('builtins.input', side_effect=_user_input):
             _Actual = Descendants.getInput()
         self.assertEqual(_Expected, _Actual)
 
-    def test_getInput_2(self):
+    def test_getInput_3(self):
         _user_input = [
-            '[{"name": "B", "parents": ["A", "C"]}, \
-            {"name": "C", "parents": ["A"]}, \
+            '[{"name": "G", "parents": ["F"]}, \
             {"name": "A", "parents": []}, \
-            {"name": "D", "parents":["C", "F"]}, \
-            {"name": "E", "parents":["D"]}, \
-            {"name": "F", "parents":[]}]']
+            {"name": "B", "parents": ["A"]}, \
+            {"name": "C", "parents": ["A"]}, \
+            {"name": "D", "parents": ["B", "C"]}, \
+            {"name": "E", "parents": ["D"]}, \
+            {"name": "F", "parents": ["D"]}, \
+            {"name": "X", "parents": []}, \
+            {"name": "Y", "parents": ["X", "A"]}, \
+            {"name": "Z", "parents": ["X"]}, \
+            {"name": "V", "parents": ["Z", "Y"]}, \
+            {"name": "W", "parents": ["V"]}]']
         _Actual = []
-        _Expected = [{"B": ["A", "C"]},
-                     {"C": ["A"]},
-                     {"A": []},
-                     {"D": ["C", "F"]},
-                     {"E": ["D"]},
-                     {"F": []}]
+        _Expected = {'G': ['F'], 'A': [], 'B': ['A'], 'C': ['A'], 'D': ['B', 'C'], 'E': ['D'], 'F': ['D'], 'X': [],
+                     'Y': ['X', 'A'], 'Z': ['X'], 'V': ['Z', 'Y'], 'W': ['V']}
         with patch('builtins.input', side_effect=_user_input):
             _Actual = Descendants.getInput()
         self.assertEqual(_Expected, _Actual)
 
     def test_getClasses_1(self):
-        _inh = [{'A': []},
-                {'B': ['A', 'C']},
-                {'C': ['A']}]
+        _inh = {'A': [],
+                'B': ['A', 'C'],
+                'C': ['A']}
         _Expected = ['A', 'B', 'C']
         _Actual = Descendants.getClasses(_inh)
         self.assertListEqual(_Expected, _Actual)
 
     def test_getClasses_2(self):
-        _inh = [{"B": ["A", "C"]},
-                {"C": ["A"]},
-                {"A": []},
-                {"D": ["C", "F"]},
-                {"E": ["D"]},
-                {"F": []}]
+        _inh = {"B": ["A", "C"],
+                "C": ["A"],
+                "A": [],
+                "D": ["C", "F"],
+                "E": ["D"],
+                "F": []}
         _Expected = ['A', 'B', 'C', 'D', 'E', 'F']
         _Actual = Descendants.getClasses(_inh)
         self.assertListEqual(_Expected, _Actual)
 
-    def test_CalculateDescendats_1(self):
-        _inh = [{'A': []}]
-        _Expected = 1
-        _Actual = Descendants.CalculateDescendats('A', _inh)
-        self.assertEqual(_Expected, _Actual)
-
-    def test_CalculateDescendats_2(self):
-        _inh = [{'A': []},
-                {'B': ['A', 'C']}]
-        _Expected = 2
-        _Actual = Descendants.CalculateDescendats('A', _inh)
-        self.assertEqual(_Expected, _Actual)
-
-    def test_CalculateDescendats_3(self):
-        _inh = [{'A': []},
-                {'B': ['A', 'C']},
-                {'C': ['A']}]
-        _Expected = 3
-        _Actual = Descendants.CalculateDescendats('A', _inh)
-        self.assertEqual(_Expected, _Actual)
-
-    def test_CalculateDescendats_4(self):
-        _inh = [{"B": ["A", "C"]},
-                {"C": ["A"]},
-                {"A": []},
-                {"D": ["C", "F"]},
-                {"E": ["D"]},
-                {"F": []}]
-        _Expected = 5
-        _Actual = Descendants.CalculateDescendats('A', _inh)
-        self.assertEqual(_Expected, _Actual)
-
-    def test_Calculate_2(self):
-        _inh = [{'A': []},
-                {'B': ['A', 'C']},
-                {'C': ['A']}]
-        _desc = ['A', 'B', 'C']
-        _Expected = {'A': 3, 'B': 1, 'C': 2}
-        _Actual = Descendants.Calculate(_inh, _desc)
+    def test_inh2graph_1(self):
+        _inh = {'A': [],
+                'B': ['A', 'C'],
+                'C': ['A']}
+        _Expected = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C']}
+        _Actual = Descendants.inh2graph(_inh)
         self.assertDictEqual(_Expected, _Actual)
 
-    def test_Calculate_1(self):
-        _inh = [{"B": ["A", "C"]},
-                {"C": ["A"]},
-                {"A": []},
-                {"D": ["C", "F"]},
-                {"E": ["D"]},
-                {"F": []}]
+    def test_inh2graph_2(self):
+        _inh = {"B": ["A", "C"],
+                "C": ["A"],
+                "A": [],
+                "D": ["C", "F"],
+                "E": ["D"],
+                "F": []}
+        _Expected = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C', 'D'], \
+                     'D': ['D', 'E'], 'E': ['E'], 'F': ['D', 'F']}
+        _Actual = Descendants.inh2graph(_inh)
+        self.assertDictEqual(_Expected, _Actual)
+
+    def test_CountDescendats_1(self):
+        _graph = {'A': ['A']}
+        _Expected = 1
+        _parent = 'A'
+        _Actual = Descendants.CountDescendats(_parent, _graph)
+        self.assertEqual(_Expected, _Actual)
+
+    def test_CountDescendats_2(self):
+        _graph = {'A': ['A', 'B'],
+                  'B': []}
+        _Expected = 2
+        _parent = 'A'
+        _Actual = Descendants.CountDescendats(_parent, _graph)
+        self.assertEqual(_Expected, _Actual)
+
+    def test_CountDescendats_3(self):
+        _graph = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C']}
+        _Expected = 3
+        _parent = 'A'
+        _Actual = Descendants.CountDescendats(_parent, _graph)
+        self.assertEqual(_Expected, _Actual)
+
+    def test_CountDescendats_4(self):
+        _graph = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C', 'D'], \
+                  'D': ['D', 'E'], 'E': ['E'], 'F': ['D', 'F']}
+        _Expected = 5
+        _parent = 'A'
+        _Actual = Descendants.CountDescendats(_parent, _graph)
+        self.assertEqual(_Expected, _Actual)
+
+    def test_Count_2(self):
+        _graph = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C']}
+        _desc = ['A', 'B', 'C']
+        _Expected = {'A': 3, 'B': 1, 'C': 2}
+        _Actual = Descendants.Count(_graph, _desc)
+        self.assertDictEqual(_Expected, _Actual)
+
+    def test_Count_1(self):
+        _graph = {'A': ['A', 'B', 'C'], 'B': ['B'], 'C': ['B', 'C', 'D'], \
+                  'D': ['D', 'E'], 'E': ['E'], 'F': ['D', 'F']}
         _desc = ['A', 'B', 'C', 'D', 'E', 'F']
         _Expected = {'A': 5,
                      'B': 1,
@@ -110,8 +125,61 @@ class TestCase_3_5_04(unittest.TestCase):
                      'D': 2,
                      'E': 1,
                      'F': 3}
-        _Actual = Descendants.Calculate(_inh, _desc)
+        _Actual = Descendants.Count(_graph, _desc)
         self.assertDictEqual(_Expected, _Actual)
+
+    def test_run_1(self):
+        _user_input = ['[{"name": "B", "parents": ["A", "C"]}, \
+        {"name": "C", "parents": ["A"]}, {"name": "A", "parents": []}, \
+         {"name": "D", "parents":["C", "F"]}, \
+          {"name": "E", "parents":["D"]}, {"name": "F", "parents":[]}]']
+        _Expected = 'A : 5\n' \
+                    'B : 1\n' \
+                    'C : 4\n' \
+                    'D : 2\n' \
+                    'E : 1\n' \
+                    'F : 3\n'
+        _Actual = None
+        with patch('builtins.input', side_effect=_user_input):
+            with patch('sys.stdout', new=StringIO()) as fake_out:
+                Descendants.run()
+                _Actual = fake_out.getvalue()
+
+        self.assertEqual(_Expected, _Actual)
+
+    def test_run_2(self):
+        _user_input = [
+            '[{"name": "G", "parents": ["F"]}, \
+            {"name": "A", "parents": []}, \
+            {"name": "B", "parents": ["A"]}, \
+            {"name": "C", "parents": ["A"]}, \
+            {"name": "D", "parents": ["B", "C"]}, \
+            {"name": "E", "parents": ["D"]}, \
+            {"name": "F", "parents": ["D"]}, \
+            {"name": "X", "parents": []}, \
+            {"name": "Y", "parents": ["X", "A"]}, \
+            {"name": "Z", "parents": ["X"]}, \
+            {"name": "V", "parents": ["Z", "Y"]}, \
+            {"name": "W", "parents": ["V"]}]']
+        _Expected = 'A : 10\n' \
+                    'B : 5\n' \
+                    'C : 5\n' \
+                    'D : 4\n' \
+                    'E : 1\n' \
+                    'F : 2\n' \
+                    'G : 1\n' \
+                    'V : 2\n' \
+                    'W : 1\n' \
+                    'X : 5\n' \
+                    'Y : 3\n' \
+                    'Z : 3\n'
+        _Actual = None
+        with patch('builtins.input', side_effect=_user_input):
+            with patch('sys.stdout', new=StringIO()) as fake_out:
+                Descendants.run()
+                _Actual = fake_out.getvalue()
+
+        self.assertEqual(_Expected, _Actual)
 
 
 if __name__ == '__main__':
--- 03/src/ex_3_5_04.py
+++ 03/src/ex_3_5_04.py
@@ -3,43 +3,49 @@ import json
 
 class Descendants:
     @staticmethod
-    def getInput() -> list:
-        s = input().rstrip()
-        _res = []
+    def getInput() -> dict:
         try:
-            _res = json.loads(s)
+            _inp_json = json.loads(input().strip())
+            return Descendants.Clean(_inp_json)
         except json.JSONDecodeError as e:
-            print("json error: ", e.msg)
-        _out = Descendants.Clean(_res)
-        return _out
+            print("json error: ", e.msg, e.pos)
 
     @staticmethod
-    def Clean(raw_inh: list) -> list:
-        _res = []
+    def Clean(raw_inh: list) -> dict:
+        _res: dict = {}
         for r in raw_inh:
-            _dict: dict = {}
-            _key: str = str()
-            for k in r.keys():
-                if k == "name":
-                    _key = r[k]
-                if k == "parents":
-                    _dict[_key] = r[k]
-            _res.append(_dict)
+            _res[r['name']] = r['parents']
         return _res
 
     @staticmethod
-    def getClasses(inh_raw: list) -> list:
+    def inh2graph(inh: dict) -> dict:
+        _graph = {}
+        _res = None
+        for ch in inh.keys():
+            if ch not in _graph.keys():
+                _graph[ch] = [ch]
+            for pr in inh[ch]:
+                if pr not in _graph.keys():
+                    _graph[pr] = [pr]
+                _graph[pr].append(ch)
+
+            _res = dict(sorted(_graph.items()))
+            for k in _res.keys():
+                _res[k].sort()
+        return _res
+
+    @staticmethod
+    def getClasses(inh: dict) -> list:
         """
-        [{"B": ["A", "C"]}]  ->  ['A','B','C']
-        :param inh_raw: list of dicts
+        {"B": ["A", "C"]}  ->  ['A','B','C']
+        :param inh: list of dicts
         :return: list
         """
         _res = set()
-        for ln in inh_raw:
-            for k in ln.keys():
-                _res.add(k)
-                for p in ln[k]:
-                    _res.add(p)
+        for k in inh.keys():
+            _res.add(k)
+            for p in inh[k]:
+                _res.add(p)
         _out = []
         for r in _res:
             _out.append(r)
@@ -47,7 +53,7 @@ class Descendants:
         return _out
 
     @staticmethod
-    def Calculate(inh: list, desc: list) -> dict:
+    def Count(inh: dict, desc: list) -> dict:
         """
         :param inh: [{"B": ["A", "C"]}]
         :param desc: ['A','B','C']
@@ -55,28 +61,52 @@ class Descendants:
         """
         _res: dict = {}
         for pr in desc:
-            _res[pr] = Descendants.CalculateDescendats(pr, inh)
+            _res[pr] = Descendants.CountDescendats(pr, inh)
         return _res
 
     @staticmethod
-    def CalculateDescendats(parent: str, inh: list) -> int:
+    def Diff(li1, li2):
+        # return list(set(li1) - set(li2))
+        li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
+        return li_dif
+
+    @staticmethod
+    def CountDescendats(parent: str, graph: dict, visited=None) -> int:
         """
         :param parent: 'A'
-        :param inh: [{"B": ["A", "C"]}]
-        :return: 1
+        :param graph: {'A': ['A','B'], 'B': []}
+        :param visited - set of visited vertexes
+        :return: 2
         """
         _sum: int = 0
-        for d in inh:  # getting dict
-            for child in d.keys():  # one key in a dict of child:[prnt1,...prntN]
-                if parent == child:
-                    _sum += 1
-                if parent in d[child]:  # check if parent in parent's list
-                    _sum += 1
-                    _sum += Descendants.CalculateDescendats(child, inh)
-                # else:
-                #     _sum += 0
+        if visited is None:
+            visited = set()
+        if parent not in visited:
+            _sum += 1
+            visited.add(parent)
+        for child in graph[parent]:
+            if child not in visited:
+                _sum += Descendants.CountDescendats(child, graph, visited)
+
+        # else:
+        #     _sum += 0
+
         return _sum
+
+    @staticmethod
+    def Print(stat: dict) -> None:
+        template = '{k} : {v}'
+        for k, v in stat.items():
+            print(template.format(k=k, v=v))
+
+    @staticmethod
+    def run():
+        _inh = Descendants.getInput()
+        _desc = Descendants.getClasses(_inh)
+        _graph = Descendants.inh2graph(_inh)
+        _stat = Descendants.Count(_graph, _desc)
+        Descendants.Print(_stat)
 
 
 if __name__ == "__main__":
-    Descendants.getInput()
+    Descendants.run()
